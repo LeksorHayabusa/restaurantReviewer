8a018bd2a6e7980cde90d32ad8d49901c46a45e7
Alex Nikonov <31586394+LeksorHayabusa@users.noreply.github.com>
2018-07-14 09:05:54 +0200

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -66,8 +66,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 80 // Change this to your server port
+    return `https://leksorhayabusa.github.io/restaurantReviewer/data/restaurants.json`;
   }
 
   /**
